1bdb7a69ca71c45e89ef8b542551af711a21337e
m-karakus <s.metinkarakus@gmail.com>
2024-12-02 02:07:09 +0300

    feat: github workflow update

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setup(
         ],
     },
     description='PostgreSQL loader for mkpipe.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Metin Karakus',
     author_email='metin_karakus@yahoo.com',
     classifiers=[
